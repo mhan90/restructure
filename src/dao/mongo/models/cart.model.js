81f5e9ab5cbf8c7ba69cdabb51316cda6455b46e
Han Marcelo <mhan@arsat.com.ar>
2023-09-29 15:25:51 -0300

    ticket model

--- src/dao/mongo/models/cart.model.js
+++ src/dao/mongo/models/cart.model.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
   products: {
     type: [
       {
