cbf7a68a89323078617daeeb5b8618f6cb83beaa
marce <marcelo.han90@gmail.com>
2023-10-09 15:05:09 -0300

    logger done

--- src/routes/loggerTest.js
+++ src/routes/loggerTest.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import EErrors from "../utils/enum.error.js";
-import logger from "../config/loggers/dev.config.js";
+import logger from "../config/loggers/logger.factory.js";
 
 const loggerTest = Router();
 
 loggerTest.get("/", (req, res) => {
     const { type } = req.query;
-    let newError = new Error("test");
-    logger[type](`[${req.method}]${req.url} - ${new Date().toUTCString()}\n ${newError}`);
-    res.json({ newError });
+    let newError = new Error(type);
+    const string = `[${req.method}]${req.url} - ${new Date().toUTCString()}\n ${newError}`;
+    logger[type](string);
+    res.send({ type: newError.message });
 });
 
 export default loggerTest;
